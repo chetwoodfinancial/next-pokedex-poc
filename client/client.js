fed221da2437bad1b5c0615421bdd3032aa89c60
Sam MacFarlane <sam.macfarlane@AS0985.local>
2022-12-20 12:33:20 +0000

    feat: Add Sanity webhook

--- client/client.js
+++ client/client.js
@@ -5,6 +5,6 @@ const projectId = process.env.NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID
 export default sanityClient({
   projectId, // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
-  useCdn: true, // `false` if you want to ensure fresh data
+  useCdn: false, // `false` if you want to ensure fresh data
   apiVersion: '2022-12-19',
 })
---
+++ client/pages/api/hook.ts
@@ -0,0 +1,56 @@
+import { isValidRequest } from "@sanity/webhook";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  message: string;
+};
+
+const secret = process.env.NEXT_PUBLIC_SANITY_STUDIO_WEBHOOK_SECRET;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method !== "POST") {
+    console.error("Must be a POST request");
+    return res.status(401).json({ message: "Must be a POST request" });
+  }
+
+  if (!isValidRequest(req, secret)) {
+    res.status(401).json({ message: "Invalid signature" });
+    return;
+  }
+
+  try {
+    const {
+      body: { type, slug },
+    } = req;
+
+    switch (type) {
+      case "post":
+        await res.revalidate(`/news/${slug}`);
+        return res.json({
+          message: `Revalidated "${type}" with slug "${slug}"`,
+        });
+    }
+
+    return res.json({ message: "No managed type" });
+  } catch (err) {
+    return res.status(500).send({ message: "Error revalidating" });
+  }
+}
+
+// // Next.js will by default parse the body, which can lead to invalid signatures
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
+
+// async function readBody(readable) {
+//   const chunks = [];
+//   for await (const chunk of readable) {
+//     chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+//   }
+//   return Buffer.concat(chunks).toString("utf8");
+// }
